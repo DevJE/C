3f2062768fc9f03d9afcda613c87c6f54c9ec3fc
macJE <jeong.dev01@gmail.com>
2020-11-21 11:58:38 +0900

    number expression

---
+++ test1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("happy \n");
+    printf("1345314399 \n");
+    printf("My\tfriend\n");
+
+    printf("Goot\bd\tchance\n");
+    printf("Cow\rW\a\n");
+
+    // 숫자 출력
+    printf("%d\n", 10);
+    printf("%lf\n", 3.4);
+    printf("%.1lf\n", 3.45);
+    printf("%.10lf\n", 3.4);
+
+    printf("%d과 %d의 합은 %d입니다.\n", 20, 20, 10 + 20);
+    printf("%.1lf - %.1lf = %.1lf\n", 3.4, 1.2, 3.4 - 1.2);
+
+    return 0;
+}
